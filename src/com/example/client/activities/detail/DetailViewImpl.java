da3e630846f9a5ff5b11cdb663518482a7d8bcdc
vumaianh <koolsoft@koolsofts-MacBook-Air.local>
2025-06-26 18:08:09 +0700

    add button home back

--- src/com/example/client/activities/detail/DetailViewImpl.java
+++ src/com/example/client/activities/detail/DetailViewImpl.java
@@ -2,12 +2,17 @@ package com.example.client.activities.detail;
 
 import com.example.client.GreetingService;
 import com.example.client.GreetingServiceAsync;
+import com.example.client.Hello_gwt;
+import com.example.client.activities.list.ListPlace;
 import com.example.shared.model.UserDTO;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
@@ -24,6 +29,9 @@ public class DetailViewImpl extends Composite implements DetailView {
 	@UiField
 	Label fullNameLabel, emailLabel, ageLabel, addressLabel, genderLabel;
 	
+	@UiField
+	Button backButton;
+	
 	
 	private UserDTO user;
 	public DetailViewImpl() {
@@ -35,6 +43,15 @@ public class DetailViewImpl extends Composite implements DetailView {
 		if (userId != null) {
 			getUserInfo(userId);
 		}
+		
+		backButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				Hello_gwt.CLIENT_FACTORY.getPlaceController().goTo(new ListPlace());
+				
+			}	
+		});
 	}
 	
 	public void setData(UserDTO user) {
